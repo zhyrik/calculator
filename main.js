e45d49e9b8d077e0edf47b6934f04e183ee792d9
Oleh <zhyriki@ukr.net>
2018-02-27 21:17:29 +0200

    finish

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
- var signs = document.querySelectorAll("input[value]");
+  var signs = document.querySelectorAll("input[value]");
  var result = document.querySelector("input#result");
 
 for( var i=0; i<signs.length; i++){
     var sign = signs[i];
-   sign.addEventListener("click", fun1);
+   sign.addEventListener("click", calculator);
 }
 
-function fun1(e){
+function calculator(e){
     var num1 = document.querySelector("input#num1").value;
     var num2 = document.querySelector("input#num2").value;
 
